bde8025d7ecc6bb7db4a4d84b28b1f1ea7423426
Evan Serrano <evanjserrano@gmail.com>
2022-03-30 20:37:51 -0700

    changed tests

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "alloc.h"
 #include <stdio.h>
 
+extern void print_heap();
+
 int main(void)
 {
     printf("Hello, World!\n");
-    void* ptr = allocm(10);
+    short* ptr = allocm(sizeof(short));
+    printf("%p\n", ptr);
+    *ptr = 0xabcd;
+    print_heap();
+    freem(ptr);
+    print_heap();
 
     return 0;
 }
